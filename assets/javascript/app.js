c9bdbd93607da40839bbb3362d885626eb7f7d9b
Blue Raptor <ciscoaravena@gmail.com>
2018-12-15 14:50:45 -0700

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,57 +11,45 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     var database = firebase.database();
 
-    var nextArrival
-    var minutesAway
+    var test
 
     $(".submit").on("click", function (event) {
         event.preventDefault();
         var name = $("#trainName").val().trim();
         var destination = $("#destination").val().trim();
-        var startTime = $("#startTime").val().trim();
+        var firstTrain = $("#firstTrain").val().trim();
         var frequency = $("#frequency").val().trim();
 
         database.ref().push({
             name: name,
             destination: destination,
-            startTime: startTime,
+            firstTrain: firstTrain,
             frequency: frequency,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         })
 
     })
-    
+
     database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
         var sv = snapshot.val();
 
 
-        //Next time is the the combination of startTime with frequency.. comparing with the current time
-        nextArrival = nextTime(snapshot.val().startTime, snapshot.val().frequency);
-        minutesAway = moment(nextArrival, "mm").fromNow(true);  
-
-
-        //template literals
         var myTD = `<tr>
         <td>${sv.name}</td>
         <td>${sv.destination}</td>
              <td>${sv.frequency}</td>
-             <td>${nextArrival}</td>
-             <td>${minutesAway}</td></tr>`
+             <td>${test}</td>
+             <td>${test}</td></tr>`
         $("tbody").append(myTD);
-    })
+    },
+    
+    
+
+    
+    )
 
 
+
+    
 });
-function nextTime(startTime, frequency) {
-    nextArrival = moment(startTime, "HH:mm").format('LT');
-    console.log(nextArrival);
-    if (moment().isBefore(nextArrival)) {
-        console.log(`time is in the past ${moment()}`);
-        return nextArrival;
-    } else {
-        while (moment().isAfter(nextArrival)) {
-            nextArrival = nextArrival.add(frequency, "m");
-        }
-    }
-    return nextArrival;
-}
+
